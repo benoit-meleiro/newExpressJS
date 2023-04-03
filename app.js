b55be1bdbacf3dbc1149bdf387a583bca7a30ec2
benoit meleiro <benoit.meleiro@gmail.com>
2023-04-03 16:46:22 +0200

    même chose mais oubli enreg

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+// je crée une variable qui utilise la méthode require pour importer le fichier mock-coworkings.js
 const coworkings = require('./mock-coworkings')
 
-
+// j'utilise get pour récupérer les données de l'api
 app.get('/api/coworkings', (req, res) => {
- 
+  // je crée une variable qui contient une chaine de caractère vide
     let sentence = ''
+     // je crée une boucle qui va parcourir le tableau coworkings
     coworkings.map((data) => {        
         sentence += data.name 
         sentence += ' - '        
@@ -17,16 +18,23 @@ app.get('/api/coworkings', (req, res) => {
     console.log(sentence);
     
 //   res.send(`Nombre de coworkings : ${coworkings.length}`);
+// je renvoie la variable sentence
     res.send(sentence)
 })
+//  j'utilise get pour récupérer les données de l'api
 app.get('/api/coworkings/:id', (req, res) => {
 
     console.log(req.params.id);
-    let myCoworking = coworkings.find((coworking) => {return coworking.id === parseInt (req.params.id)})
+    // je crée une variable qui va chercher dans le tableau coworkings l'id correspondant à l'id de l'url
+    let myCoworking = coworkings.find((coworking) => {
+         // je parse l'id de l'url en int pour pouvoir le comparer à l'id du tableau
+    // le parseInt sert à convertir une chaine de caractère en nombre
+    // le parseInt est nécessaire car l'id de l'url est une chaine de caractère
+    return coworking.id === parseInt (req.params.id)})
+
     res.send(`Nom du coworking :  ${myCoworking.name} !`)
     })
   
-
 
 app.listen(port, () => {
   console.log(`elle écoute suur le port ${port}`)
